fff32f48b93b7d5c1e5cf5cec799b59a1e38cb1d
ltdinh <ltdinh@enjoyworks.co.kr>
2023-02-07 13:39:39 +0700

    init source code java

---
+++ laundry_api/src/main/java/laundry/com/laundry_api/LaundryApiApplication.java
@@ -0,0 +1,13 @@
+package laundry.com.laundry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryApiApplication.class, args);
+	}
+
+}
---
+++ laundry_api/src/test/java/laundry/com/laundry_api/LaundryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package laundry.com.laundry_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
