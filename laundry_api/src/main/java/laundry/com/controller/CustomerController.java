3f54265e1537e4054dc7ff6b8b85a6826999a88f
ltdinh <ltdinh@enjoyworks.co.kr>
2023-02-08 13:32:57 +0700

    update api

--- laundry_api/src/main/java/laundry/com/controller/CustomerController.java
+++ laundry_api/src/main/java/laundry/com/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package laundry.com.controller;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +19,14 @@ public class CustomerController {
 	private CustomerService customerService;
 	
 	@PostMapping("/laundry/customers")
-	public void create(@RequestBody Map<String, String> customer) {
+	public ResponseEntity<?> create(@RequestBody Map<String, String> customer) {
 		customerService.insert(customer);
+		
+		Map<String, Object> data = new LinkedHashMap<>();
+		data.put("timestamp", System.currentTimeMillis());
+		data.put("status", HttpStatus.OK.value());
+		data.put("message", "Insert customer successfully!");
+		return new ResponseEntity<>(data, HttpStatus.OK);
 	}
 
 }
